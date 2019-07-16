e07dafd9d103f6fcc221b918b167310995172b16
Steve <smather@astev.com>
2019-07-17 09:17:22 +1000

    generate build files

---
+++ components/form/CheckBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button class="btn" @click="swapLabel">{{test}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: 'Test'
+    }
+  },
+  methods: {
+    testLog() {
+      console.log('test')
+    },
+    swapLabel() {
+      console.log('swap label')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
